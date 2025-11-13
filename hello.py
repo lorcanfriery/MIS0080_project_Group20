b06d745d3b309629c3759401965682909a9f99fe
Odhran McLaughlin <odhranmc@Odhrans-MacBook-Air.local>
2025-11-13 14:59:36 +0000

    Added Fred Api

--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
 print("hello world")
 print("hello again")
+print("new")
+
+
+# Testing FREDAPI
+from fredapi import Fred
+import pandas as pd
+
+fred = Fred(api_key="e5f01fd50421590fffaebf8fec19052a")
+
+eurusd = fred.get_series("DEXUSEU", observation_start="2015-01-01")
+eurusd.name = "EURUSD"
+print(eurusd.head())
