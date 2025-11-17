f3dac8f60eda60065ebc01e55acf050abc5a99c1
lorcanfriery <lorcan.friery@ucdconnect.ie>
2025-11-17 12:22:46 +0000

    Added a comparison feature

--- DATA/dashboard_app.py
+++ DATA/dashboard_app.py
@@ -12,7 +12,12 @@ st.line_chart(filtered.set_index("year")[metric])
 years = st.slider("Select year range:", int(df["year"].min()), int(df["year"].max()), (2000, 2024))
 filtered=filtered[(filtered["year"] >= years[0]) & (filtered["year"] <= years[1])]
 
-
-
+compare =st.checkbox("Compare with another countyry")
+if compare:
+    country2 = st.selectbox("Select another country:", sorted(df["country"].unique()),key="compare")
+    filtered2 = df[(df["country"] == country2) 
+                   
+    st.line_chart({country: filtered.set_index("year")[metric],
+                   country2: filtered2.set_index("year")[metric]})
 
 
